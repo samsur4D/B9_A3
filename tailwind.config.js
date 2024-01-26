e9cb90c5ee57c053168ce4484bdc9b57a9222d5a
samsur4D <patarehan14600@gmail.com>
2024-01-27 04:34:41 +0600

    add nav

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            black: "#131318",
+            gray: "#131318",
+            red: "#FF4240",
+            white: "#ffffff"
+        },
+      extend: {},
+    },
+    plugins: [],
+  }
